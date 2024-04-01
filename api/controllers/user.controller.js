c5cd51e6fbefd520b1f662e09f1652e44f2be2c9
18Clouds <vanbagga@gmail.com>
2024-04-01 23:10:33 +0530

    Completed update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,39 +1,93 @@
-import { errorHandler } from "../utils/error.js";
-import bcryptjs from "bcryptjs";
-import User from "../models/user.model.js";
+// import { errorHandler } from "../utils/error.js";
+// import bcryptjs from "bcryptjs";
+// import User from "../models/user.model.js";
 
-const test = (req, res) => {
-  res.json({ message: "API IS WORKING" });
-};
+// export const test = (req, res) => {
+//   res.json({ message: "API IS WORKING" });
+// };
 
-const train = (req, res) => {
+export const train = (req, res) => {
   res.json({ message: "train" });
 };
 
-const updateUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId)
+// const updateUser = async (req, res, next) => {
+//   if (req.user.id !== req.params.userId)
+//     return next(errorHandler(403, "You are not allowed to update this user"));
+//   if (req.body.password) {
+//     if (req.body.password.length < 6)
+//       return next(errorHandler(400, "Password must be atleast 6 characters"));
+//     req.body.password = bcryptjs.hashSync(req.body.password, 10);
+//   }
+//   if (req.body.username) {
+//     if (req.body.username.length < 7 || req.body.username.length > 20)
+//       return next(
+//         errorHandler(
+//           400,
+//           "username should be more than 7 and less than 20 characters"
+//         )
+//       );
+//     if (req.body.username.includes(" "))
+//       return next(errorHandler(400, "username must not contain a space"));
+//     if (req.body.username !== req.body.username.toLowerCase())
+//       return next(errorHandler(400, "username must be lowercase"));
+//     if (!req.body.username.match(/^[a-zA-Z0-9]+$/))
+//       return next(
+//         errorHandler(400, "username must only contain letters and numbers")
+//       );
+//   }
+//   try {
+//     const updatedUser = await User.findByIdAndUpdate(
+//       req.params.userId,
+//       {
+//         $set: {
+//           username: req.body.username,
+//           email: req.body.email,
+//           password: req.body.password,
+//           profilePicture: req.body.profilePicture,
+//         },
+//       },
+//       { new: true }
+//     );
+//     const { password, ...rest } = updatedUser._doc;
+//     res.status(200).json(rest);
+//   } catch (error) {}
+//   console.log(req.user);
+// };
+// export { test, train, updateUser };
+
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import User from "../models/user.model.js";
+export const test = (req, res) => {
+  res.json({ message: "API is working!" });
+};
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, "You are not allowed to update this user"));
+  }
   if (req.body.password) {
-    if (req.body.password.length < 6)
-      return next(errorHandler(400, "Password must be atleast 6 characters"));
+    if (req.body.password.length < 6) {
+      return next(errorHandler(400, "Password must be at least 6 characters"));
+    }
     req.body.password = bcryptjs.hashSync(req.body.password, 10);
   }
   if (req.body.username) {
-    if (req.body.username.length < 7 || req.body.username.length > 20)
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
       return next(
-        errorHandler(
-          400,
-          "username should be more than 7 and less than 20 characters"
-        )
+        errorHandler(400, "Username must be between 7 and 20 characters")
       );
-    if (req.body.username.includes(" "))
-      return next(errorHandler(400, "username must not contain a space"));
-    if (req.body.username !== req.body.username.toLowerCase())
-      return next(errorHandler(400, "username must be lowercase"));
-    if (!req.body.username.match(/^[a-zA-Z0-9]+$/))
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
       return next(
-        errorHandler(400, "username must only contain letters and numbers")
+        errorHandler(400, "Username can only contain letters and numbers")
       );
+    }
   }
   try {
     const updatedUser = await User.findByIdAndUpdate(
@@ -42,15 +96,15 @@ const updateUser = async (req, res, next) => {
         $set: {
           username: req.body.username,
           email: req.body.email,
-          password: req.body.password,
           profilePicture: req.body.profilePicture,
+          password: req.body.password,
         },
       },
       { new: true }
     );
     const { password, ...rest } = updatedUser._doc;
     res.status(200).json(rest);
-  } catch (error) {}
-  console.log(req.user);
+  } catch (error) {
+    next(error);
+  }
 };
-export { test, train, updateUser };
--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -1,13 +1,44 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
+// const initialState = {
+//   currentUser: null,
+//   error: null,
+//   loading: false,
+// };
+
+// const userSlice = createSlice({
+//   name: "user",
+//   initialState,
+//   reducers: {
+//     signInStart: (state) => {
+//       state.loading = true;
+//       state.error = null;
+//     },
+//     signInSuccess: (state, action) => {
+//       state.currentUser = action.payload;
+//       state.error = null;
+//       state.loading = false;
+//     },
+//     signInFailure: (state, action) => {
+//       state.currentUser = action.payload;
+//       //   state.error = action.error;
+//       state.loading = false;
+//     },
+//   },
+// });
+
+// export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+// export default userSlice.reducer;
+
+
+import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   currentUser: null,
   error: null,
   loading: false,
 };
-
 const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState,
   reducers: {
     signInStart: (state) => {
@@ -16,16 +47,36 @@ const userSlice = createSlice({
     },
     signInSuccess: (state, action) => {
       state.currentUser = action.payload;
-      state.error = null;
       state.loading = false;
+      state.error = null;
     },
     signInFailure: (state, action) => {
-      state.currentUser = action.payload;
-      //   state.error = action.error;
       state.loading = false;
+      state.error = action.payload;
+    },
+    updateStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    updateSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = null;
+    },
+    updateFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
     },
   },
 });
 
-export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+export const {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+  updateStart,
+  updateSuccess,
+  updateFailure,
+} = userSlice.actions;
+
 export default userSlice.reducer;
