a741cbc0ee31e042ac1a9bd546d30ecbe533b2b0
18Clouds <vanbagga@gmail.com>
2024-04-08 20:27:53 +0530

    Created get posts api route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,11 +1,10 @@
-import express from 'express';
-import { verifyUser } from '../utils/verifyUser.js';
-import { create } from '../controllers/post.controller.js';
+import express from "express";
+import { verifyUser } from "../utils/verifyUser.js";
+import { create, getposts } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-
-
-router.post('/create', verifyUser, create);
+router.post("/create", verifyUser, create);
+router.get("/getposts", getposts);
 
 export default router;
