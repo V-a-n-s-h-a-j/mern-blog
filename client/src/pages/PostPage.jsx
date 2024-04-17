e37c3c56bcae28039c07baaf167b3c6467db8779
18Clouds <vanbagga@gmail.com>
2024-04-17 11:23:55 +0530

    Created the post page and added call to action to the post page

---
+++ client/src/pages/PostPage.jsx
@@ -0,0 +1,61 @@
+import { Button, Spinner } from 'flowbite-react';
+import React, { useState, useEffect } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import CallToAction from '../components/CallToAction';
+
+export default function PostPage() {
+    const {postSlug} = useParams();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [post, setPost] = useState(null);
+    useEffect(()=>{
+        try {
+            const fetchPosts = async() => {
+                const res = await fetch(`/api/post/getposts?slug=${postSlug}`);
+                const data = await res.json();
+                if (!res.ok)
+                {
+                    setLoading(false);
+                    setError(data.message);
+                }
+                else {
+                    setLoading(false);
+                    setPost(data.posts[0]);
+                }
+            }
+            fetchPosts();
+        } catch (error) {
+            setError(true);
+            setLoading(false);
+            console.log(error.message)    
+        }
+    }, [postSlug])
+    console.log(post);
+
+
+  if (loading)
+  {
+    return <div className='flex justify-center items-center min-h-screen'>
+        <Spinner size='xl'/>
+    </div>
+  }
+  return (
+    <main className='p-3 flex flex-col max-w-6xl mx-auto min-h-screen'>
+        <h1 className='text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl'>{post && post.title}</h1>
+        <Link to={`/search?category=${post.category}`}
+        className='self-center mt-5 '
+        >
+            <Button color='gray' size='xs' pill>{post && post.category}</Button>
+        </Link>
+        <img src={post && post.image} alt={post && post.title} className='mt-10 p-3 max-h-[600px] w-full object-cover'/>
+        <div className='flex justify-between p-3 border-b border-slate-500 mx-auto w-full max-w-2xl text-xs'>
+            <span>{post && new Date(post.createdAt).toLocaleDateString()}</span>
+            <span>{post && (post.content.length / 1000).toFixed(0)}mins read</span>
+        </div>
+        <div className="p-3 max-w-2xl mx-auto w-full post-content" dangerouslySetInnerHTML={{__html: post && post.content}}></div>
+        <div className='max-w-4xl mx-auto w-full'>
+            <CallToAction/>
+        </div>
+    </main>
+  )
+}
